559deb4dcfbf2348395359492e42fe5892456422
Денис Зубрицкий <fort_zd@mail.ru>
2022-09-15 06:56:42 +0700

    Исправил в соответствии с правильным решением задания #1 - определить функции handleIncrement и handleDecrement в родительском компоненте countersList

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -2,57 +2,54 @@ import React, { useState } from "react";
 import Counter from "./counter";
 
 const CountersList = () => {
-  const initialState = [
-    { id: 1, value: 0, name: "Ненужная вещь" },
-    { id: 2, value: 5, name: "Ложка" },
-    { id: 3, value: 0, name: "Вилка" },
-    { id: 4, value: 0, name: "Тарелка" },
-    { id: 5, value: 0, name: "Набор минималиста" },
-  ];
+    const initialState = [
+        { id: 0, value: 0, name: "Ненужная вещь", price: "200" },
+        { id: 1, value: 4, name: "Ложка" },
+        { id: 2, value: 0, name: "Вилка" },
+        { id: 3, value: 0, name: "Тарелка" },
+        { id: 4, value: 0, name: "Набор минималиста" },
+    ];
 
-  const [counters, setCounters] = useState(initialState);
+    const [counters, setCounters] = useState(initialState);
+    const handleDelete = (id) => {
+        const newCounters = counters.filter((c) => c.id !== id);
+        setCounters(newCounters);
+    };
+    const handleReset = () => {
+        setCounters(initialState);
+        console.log("handle reset");
+    };
+    const handleIncrement = (id) => {
+        const elementIndex = counters.findIndex((c) => c.id === id);
+        const newCounters = [...counters];
+        newCounters[elementIndex].value++;
+        setCounters(newCounters);
+    };
+    const handleDecrement = (id) => {
+        const elementIndex = counters.findIndex((c) => c.id === id);
+        const newCounters = [...counters];
+        newCounters[elementIndex].value--;
+        setCounters(newCounters);
+    };
 
-  const handleDelete = (id) => {
-    const newCounter = counters.filter((c) => c.id !== id);
-    setCounters(newCounter);
-  };
-
-  const handleReset = () => {
-    setCounters(initialState);
-  };
-
-  const handleIncrement = (id) => {
-    let newValues = [...counters];
-    newValues.forEach((item) => {
-      item.id === id && item.value++;
-    });
-    setCounters(newValues);
-  };
-
-   const handleDecrement = (id) => {
-    let newValues = [...counters];
-    newValues.forEach((item) => {
-      item.id === id && item.value--;
-    });
-    setCounters(newValues);
-  }; 
-
-  return (
-    <>
-      {counters.map((count) => (
-        <Counter
-          key={count.id}
-          onDelete={handleDelete}
-          onIncrement={handleIncrement}
-          onDecrement={handleDecrement}
-          {...count}
-        />
-      ))}
-      <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
-        Сброс
-      </button>
-    </>
-  );
+    return (
+        <>
+            {counters.map((count) => (
+                <Counter
+                    key={count.id}
+                    onDelete={handleDelete}
+                    onIncrement={handleIncrement}
+                    onDecrement={handleDecrement}
+                    {...count}
+                />
+            ))}
+            <button
+                className='btn btn-primary btn-sm m-2'
+                onClick={handleReset}
+            >
+                Сброс
+            </button>
+        </>
+    );
 };
-
 export default CountersList;
